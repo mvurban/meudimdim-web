b9de0f73185ab1eef6258951a89fb2491c7f42bb
mvurban <mvurban@mvurban.eti.br>
2026-03-18 20:47:02 +0000

    ajuste pra build na vercel

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/**',
+      'node_modules/webpack/**',
+      'node_modules/terser/**',
+      'node_modules/esbuild/**',
+      'node_modules/rollup/**',
+      'node_modules/typescript/**',
+      'node_modules/prettier/**',
+    ],
+  },
 }
 
 module.exports = nextConfig
